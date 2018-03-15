983dcf2fb585da6ddee1f5db26f8eaab814c0963
JimKass <james.kastner@my.jcu.edu.au>
2018-03-15 18:57:10 +1000

    Add __setitem__ and __getitem__

--- sparse-array.py
+++ sparse-array.py
@@ -6,20 +6,38 @@ class SparseArray:
 
     class _Node:
         """A non_public used for storing elements in the SparseArray."""
-        def __init__(self, _e, _i, _next):
+        def __init__(self, _e, _i, _next, _prev):
             self._e = _e
             self._i = _i
             self._next = _next
+            self._prev = _prev
 
     def __init__(self, n):
         self.m = 0
         self.n = n
-        self.tail = self._Node(None, None, None)
-        self.head = self._Node(None, None, self.tail)
+        self.tail = self._Node(None, None, None, None)
+        self.head = self._Node(None, None, None, None)
+        self.head._next = self.tail
+        self.tail._prev = self.head
 
     def get_usage(self):
         """Prints the amount of elements in the SparseArray."""
         return self.m
+
+    def _get_node_at(self, i, node):
+        """A recursive function that will return the node at index i."""
+        if node is self.tail:
+            return None
+        elif node._i == i:
+            return node
+        else:
+            return self._get_node_at(i, node._next)
+
+    def _make_node(self, e, i):
+        """Makes a new node containing element e at index i"""
+        new_node = self._Node(e, i, self.tail, self.tail._prev)
+        self.tail._prev._next = new_node
+        self.tail._prev = new_node
 
     def __len__(self):
         """Prints the capacity of the SparseArray."""
@@ -29,11 +47,17 @@ class SparseArray:
         """Returns the value of the element at index i."""
         if i > self.n:
             raise IndexError
+        return self._get_node_at(i, self.head._next)
 
     def __setitem__(self, j, e):
         """Sets the value of element at index j to e."""
         if j > self.n:
             raise IndexError
+        node = self._get_node_at(j, self.head._next)
+        if node == None:
+            self._make_node(e, j)
+        else:
+            node._e = e
 
     def fill(self, seq):
         """Add all of the elements from sequence seq into the SparseArray."""
